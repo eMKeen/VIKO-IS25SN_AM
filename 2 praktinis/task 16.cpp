a7539b56d33cbed5558659890303a5020bbfd91f
Aleksandras <95692027+eMKeen@users.noreply.github.com>
2026-03-06 11:10:49 +0200

    Ivedimo dalys, ciklas ir testinumas leneteles

--- 2 praktinis/task 16.cpp
+++ 2 praktinis/task 16.cpp
@@ -22,7 +22,144 @@ Reikalavimai:
 #include <string>
 #include <format>
 #include <iomanip>
+#include <iterator>
 using namespace std;
+
+int ilgiausiaiTekstas(string _tekstoMasyvas[],int n);
+void tbl(int _tekstoIlgis, string _text, int _ilgisLenget, int _eilutesNum, int _kitaEilute);
+
+
+int task16() {
+    // --Masyvai ir kiti--
+    string MV[100]; // mokyniu Vardu ir Pavardziu masyvai
+    int _uzmt = 0;// index`o uzimtumas
+    int PZ[100][10]={0}; //pazymiu masyvas
+    string dalykai[10] =
+    {
+        "Matematika", "Fizika", "Programavimas", "Biologija", "Kuno kultura",
+        "Geografija", "Istorija", "Gimtoji kalba", "Anglu kalba", "Dorinis ugdymas"
+        };
+
+    int m,p,kt,ms,ms1; // m - mokyniu index`as <> p - pazymiu index`as <> kt - keisti arba trinti index`as
+    int i,j,l;
+    int _tekstoIlgis; // Teksto ilgis
+
+    string _text;
+
+    while (true) {
+
+
+        // --- Startas ---
+        _text="Mokiniu pazymiu sistema";
+        _tekstoIlgis=_text.length();
+        tbl(_tekstoIlgis,_text,1,1,-1);
+
+        //---Meniu---
+
+         string MS[] = {
+             "Pasirinkite norima operacija:",
+             "Ivesti mokiniu vardus ir ju pazymius - 1 ",
+             "Perziureti visu mokiniu pazymius - 2 ",
+             "Perziureti mokinio pazymius - 3 ",
+             "Atnaujinti moyknio pazymi - 4 ",
+             "Pasalinti mokini is saraso - 5 ",
+             "Nutraukti programa - 0 "
+         };
+
+        int _menuPasir = size(MS);                       //Masyvo ilgis
+        _tekstoIlgis = ilgiausiaiTekstas(MS, _menuPasir);// Tikrinimas ilgiausio elemento (vnt)
+
+        int _menuLines = _menuPasir + 3; // Meniu eiluciu kiekis
+        for (l=1;l<=_menuLines;l++) {
+            if (l == 1 || l == 3 || l == _menuLines) {
+                    tbl(_tekstoIlgis,"=",_menuLines,l,0);
+                } else if ( l == 2) {
+                    tbl(_tekstoIlgis,MS[l-2],_menuLines,l,1);
+                } else if (l > 3) {
+                    tbl(_tekstoIlgis,MS[l-3],_menuLines,l,1);
+            }
+        }
+
+        //---Ivestis Meniu----
+        cin >> ms;
+
+        while (true) {
+            switch (ms) {
+                case 0: {
+                    return 0;
+                }
+                case 1: {
+                    //--- Select 1 Meniu -----
+                    _text = format("Iveskite kiek mokyniu norite ivesti | Limitas - {}, jau ivesta - {}", 100,_uzmt);
+                    _tekstoIlgis=_text.length();
+                    tbl(_tekstoIlgis,_text,1,1,-1);
+                    cin>>ms1;
+                    while (ms1 <1 || ms1>100-_uzmt) {
+                        _text = format("Ivedimo intervalas nuo 1 iki {}, pakartokite:",100-_uzmt);
+                        _tekstoIlgis=_text.length();
+                        tbl(_tekstoIlgis,_text,1,1,-1);
+                        cin>>ms1;
+                    }
+                    if (_uzmt != 0) {
+                        ms1 =  ms1 + _uzmt;
+                    }
+                    for ( m=_uzmt; m<ms1; m++) {
+                        cout << format("<<===================>> \n"
+                        "Iveskite mokinio varda : ");
+                        cin>>MV[m];
+                        _uzmt++;
+                        for (p=0; p<10; p++) {
+                            cout << format("{} - Iveskite pazymi, dalikas: {} : ",p+1,dalykai[p]);
+                            cin>>PZ[m][p];
+                            while (PZ[m][p] < 1 || PZ[m][p] > 10) {
+                                cout<<"! - Blogai ivestas pazymys, galimi variantai nuo 1 iki 10. Pakartotike : ";
+                                cin>>PZ[m][p];
+                            }
+                        }
+                    }
+                }break;
+                case 2: {
+
+                }break;
+                case 3: {
+
+                }break;
+                case 4: {
+
+                }break;
+                case 5: {
+
+                }break;
+                default: {
+                    _text="Tokio pasirinkimo nera, pakartokite";
+                    _tekstoIlgis=_text.length();
+                    tbl(_tekstoIlgis,_text,1,1,-1);
+                    cin>>ms;
+                }break;
+            }break;
+        }
+
+
+        //---Pazymiu Lentele---
+
+        //----Pabaiga----
+
+        cout<<endl;
+        cout<<"--"<<endl;
+        //return 0;
+    }
+}
+
+int ilgiausiaiTekstas(string _tekstoMasyvas[],int n) {  // Tikrinam ilgiausia teksta
+
+    int _tekstoIlgis = 0;
+    for (int i=0; i < n; i++) {
+        if (_tekstoIlgis<_tekstoMasyvas[i].length()) {
+            _tekstoIlgis = _tekstoMasyvas[i].length();
+        }
+    }
+    return _tekstoIlgis;
+}
 
 void tbl(int _tekstoIlgis, string _text, int _ilgisLenget, int _eilutesNum, int _kitaEilute) {
     int i,l;
@@ -47,74 +184,12 @@ void tbl(int _tekstoIlgis, string _text, int _ilgisLenget, int _eilutesNum, int 
             }
             cout<<endl;
         } else {
-            if (_kitaEilute == 1) {
+            if (_kitaEilute == 0) {
                 cout<<"|  "<<setw(_tekstoIlgis)<<_text<<"   |";
             } else {
                 cout<<"|  "<<setw(_tekstoIlgis)<<_text<<"   |";
                 cout<<endl;
             }
         }
-    }
-}
-
-int task16() {
-    while (true) {
-
-        // --Masyvai--
-        string MV[100]; // mokyniu Varbu ir Pavardziu masyvai
-        int PZ[100][10]={0}; //pazymiu masyvas
-        string dalykai[10] =
-            {
-            "Matematika", "Fizika", "Programavimas", "Biologija", "Kuno kultura",
-            "Geografija", "Istorija", "Gimtoji kalba", "Anglu kalba", "Dorinis ugdymas"
-            };
-
-        int m,p,kt; // m - mokyniu index`as <> p - pazymiu index`as <> kt - keisti arba trinti index`as
-        int i,j,l;
-        int _tekstoIlgis; // Teksto ilgis
-
-        string _text;
-
-        // --- Startas ---
-        _text="Mokiniu pazymiu sistema";
-        _tekstoIlgis=_text.length();
-        tbl(_tekstoIlgis,_text,1,1,-1);
-
-        //---Meniu---
-
-         string MS[] = {
-             "Pasirinkite norima operacija:",
-             "Ivesti mokiniu vardus ir ju pazymius - 1 ",
-             "Perziureti visu mokiniu pazymius - 2 ",
-             "Perziureti mokinio pazymius - 3 ",
-             "Atnaujinti moyknio pazymi - 4 ",
-             "Pasalinti mokini is saraso - 5 "
-         };
-
-         int menuPasir = 6; // Meniu pasirinkimas
-         int menuLines = menuPasir + 3; // Meniu eiluciu kiekis
-        _tekstoIlgis = 0;
-        for (i=0;i<menuPasir;i++) {
-            if (_tekstoIlgis<MS[i].length()) {
-                _tekstoIlgis = MS[i].length(); // Tikrinam ilgiausia teksta
-            }
-        }
-
-        for (l=1;l<=menuLines;l++) {
-            if (l == 1 || l == 3 || l == menuLines) {
-                    tbl(_tekstoIlgis,"=",menuLines,l,1);
-                } else if ( l == 2) {
-                    tbl(_tekstoIlgis,MS[l-2],menuLines,l,0);
-                } else if (l > 3) {
-                    tbl(_tekstoIlgis,MS[l-3],menuLines,l,0);
-            }
-        }
-
-
-
-        //---Pazymiu Lentele---
-        cout<<endl;
-        cout<<"--"<<endl;
-        return 0;
     }
 }
